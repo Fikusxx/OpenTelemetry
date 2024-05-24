0e11603daef86100dacb3fcfbcd8bf5dc9d830cc
Fikusx <ggl_91@mail.ru>
2024-05-24 12:35:53 +0300

    new envs && MT support

--- OpenTelemetryTest/Program.cs
+++ OpenTelemetryTest/Program.cs
@@ -1,3 +1,5 @@
+using MassTransit.Logging;
+using MassTransit.Monitoring;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
@@ -11,21 +13,28 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddOpenTelemetry()
-    .ConfigureResource(options =>
-    {
-        options.AddService(serviceName: Diagnostics.ServiceName, serviceVersion: "1.0");
-    })
+    // .ConfigureResource(options =>
+    // {
+    //     options.AddService(serviceName: Diagnostics.ServiceName, serviceVersion: "1.0");
+    //     options.AddAttributes(new List<KeyValuePair<string, object>>
+    //     {
+    //         new("my-attr", "my-value")
+    //     });
+    // })
     .WithMetrics(metrics =>
     {
         metrics.AddAspNetCoreInstrumentation()
             .AddHttpClientInstrumentation();
 
         metrics.AddMeter(Diagnostics.Meter.Name);
+        metrics.AddMeter(InstrumentationOptions.MeterName);
     })
     .WithTracing(tracing =>
     {
         tracing.AddAspNetCoreInstrumentation()
             .AddHttpClientInstrumentation();
+        
+        tracing.AddSource(DiagnosticHeaders.DefaultListenerName);
     });
 
 builder.Logging.AddOpenTelemetry(logging =>
